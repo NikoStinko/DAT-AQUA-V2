6c86f71dd6db55e9b34ddcda49c5eeb992b07271
NikoStinko <niko.stinko64@gmail.com>
2025-03-20 00:38:25 +0100

    Add files via upload

---
+++ upload-from-arduino.ino
@@ -0,0 +1,76 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = ">your_SSID<";
+const char* password = ">your_PASSWORD<";
+
+const char* serverUrl = "http://dat-aqua.lbtrd.com/api.php";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String postData = "data=" + urlEncode("Vos données ici"); // Exemple de données à envoyer
+
+    int httpResponseCode = http.POST(postData);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  }
+
+  int delay = 60000; // Attendre une minute avant d'envoyer à nouveau, on peut changer la valeur
+
+  delay(delay);
+}
+
+String urlEncode(String str) {
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    for (int i = 0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c >9){
+            code0=c - 10 + 'A';
+        }
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+      }
+      yield();
+    }
+    return encodedString;
+}
